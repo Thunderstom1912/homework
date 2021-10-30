d4e5fa600cdf6b0049f6e752622dad44396a739a
Thunderstom1912 <73483996+Thunderstom1912@users.noreply.github.com>
2021-10-30 15:56:31 +0700

    Create do_F_sang_do_C.c

---
+++ do_F_sang_do_C.c
@@ -0,0 +1,16 @@
+// Đề bài: Chuyển độ F sang độ C.
+
+int main() {
+	int F,C;
+	
+	printf("Xin chao\n");
+	printf("Hay nhap do F: ");
+	scanf("%d",&F);
+	printf("\n");
+	
+	C = (5*(F-32))/9;
+	
+	printf("Do C la: %d",C);
+	
+    return 0;
+}
